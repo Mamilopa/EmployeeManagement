d5c05946cc81f7dae8ac21ef29d2693e78cbd9b2
Pauline Risbecker <Pauline@Risbecker.se>
2017-12-20 16:21:06 +0100

    first commit

---
+++ EmployeeManagementServerApplication/src/com/virtualpairprogrammers/employeemanagement/EmployeeManagementImplementation.java
@@ -0,0 +1,35 @@
+package com.virtualpairprogrammers.employeemanagement;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.ejb.Stateless;
+
+import com.virtualpairprogrammers.employeemanagement.domain.Employee;
+
+@Stateless
+public class EmployeeManagementImplementation implements EmployeeManagementService {
+
+	@Override
+	public void registerEmployee(Employee employee) {
+			}
+
+	@Override
+	public List<Employee> getAllEmployees() {
+		Employee e1= new Employee("James", "Green","Writer",1700);
+		Employee e2= new Employee("Sharon", "White","Editor",2100);
+		Employee e3= new Employee("Darren", "Blue","Manager",31250);
+		
+		List<Employee> employees= new ArrayList<Employee>();
+		employees.add(e1);
+		employees.add(e2);
+		employees.add(e3);
+		return employees;
+	}
+
+	@Override
+	public List<Employee> searchBySurname(String surname) {
+		return null;
+	}
+
+}
